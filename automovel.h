eceec7416b40a09e281b19555965ef70c79fae2b
Julio Cesar Filho <juliomaiaf@live.com>
2019-09-09 14:03:44 -0300

    Update automovel.h

--- automovel.h
+++ automovel.h
@@ -1,29 +1,15 @@
-#include <iostream>
-#include <string>
-
-
-class automovel{
-	private: 
-	string marca; 
-	float preco; 
-	string chassi; 
-	int fab_data;
-	int fab_dia;
-	int fab_mes;
-
-	public: 
-	string getMarca(); 
-	string setMarca(string m); 
-	float getPreco(); 
-	float setPreco(float p);
-	string getChassi(); 
-	string setChassi(string s);
-	int getFab_data(); 
-	int setFab_data(int fd);
-	int getFab_dia(); 
-	int setFab_dia(int fdi);
-	int getFab_mes(); 
-	int setFab_mes(int fmes);
+std::ostream& operator<< (std::ostream &o, Empresa &empresa){
+	for (std::vector<Funcionario*>::iterator i = empresa.lista_funcionarios.begin(); i != empresa.lista_funcionarios.end(); ++i)
+	{
+		o << (**i) << std::endl;
+	}
+	return o;
 }
 
-
+bool Empresa::operator==(const Empresa &empr) const{
+	if (this->nome == empr.nome){
+		return true;
+	} else {
+		return false;
+	}
+}
